d14ae71ab5e4019ac9fb1c8ed3f36f632ed3c488
MiguelABatista <miguel260503@gmail.com>
2026-05-25 18:51:47 -0300

    sadasd

--- trabalhoGrupo2/test_gap.jl
+++ trabalhoGrupo2/test_gap.jl
@@ -1,9 +1,9 @@
 using AssignmentProblems
 using Printf
 
-for inst in instances(Instance)
-    data = loadAssignmentProblem(inst)
+for inst in instances(AssignmentProblems.Instance)
+    data = loadAssignmentProblem(Symbol(inst))
     data === nothing && continue
-    @printf "%-15s  agentes=%3d  jobs=%4d  lb=%8d  ub=%8d\n" \
-        data.name na(data) nj(data) data.lb data.ub
+    @printf("%-15s  agentes=%3d  jobs=%4d  lb=%8d  ub=%8d\n",
+        data.name, na(data), nj(data), data.lb, data.ub)
 end
